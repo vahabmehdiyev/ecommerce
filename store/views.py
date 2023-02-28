b6b88a95abc6e0f813a07be8a39949f5e514a2de
Vahab Mehdiyev <vahab3089@gmail.com>
2023-03-01 02:43:45 +0400

    Second Commit

--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    context = dict()
+    return render(request, 'store.html', context)
+
+def card(request):
+    context = dict()
+    return render(request, 'cart.html', context)
+
+def checkout(request):
+    context = dict()
+    return render(request, 'checkout.html', context)
+
+
